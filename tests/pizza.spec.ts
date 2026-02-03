8ee968e7f6c38ec1a2b90dbbe34ed8bf9159d497
LukeChamberlain <LukeChamberlain64@gmail.com>
2026-02-03 10:21:45 -0700

    created purchase with login test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -5,3 +5,26 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test("purchase with login", async ({ page }) => {
+  await page.locator("body").click();
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).click();
+  await page
+    .getByRole("textbox", { name: "Email address" })
+    .fill("test@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("test");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("button", { name: "Order now" }).click();
+  await page.getByRole("combobox").selectOption("18");
+  await page.getByRole("link", { name: "Image Description Pepperoni" }).click();
+  await page.getByRole("link", { name: "Image Description Veggie A" }).click();
+  await page.getByRole("link", { name: "Image Description Margarita" }).click();
+  await page.getByRole("link", { name: "Image Description Crusty A" }).click();
+  await page.getByRole("link", { name: "Image Description Charred" }).click();
+  await page.getByRole("button", { name: "Checkout" }).click();
+  await page.getByRole("button", { name: "Pay now" }).click();
+  await page.getByRole("button", { name: "Verify" }).click();
+  await page.getByRole("button", { name: "Close" }).click();
+});
